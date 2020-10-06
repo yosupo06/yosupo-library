86c0749e3387567112b4f12ec27af11125d50891
Kohei Morita <yosupo06@gmail.com>
2020-10-07 00:34:39 +0900

    fix bit

--- test/unittest/bit_test.cpp
+++ test/unittest/bit_test.cpp
@@ -7,6 +7,38 @@ using namespace yosupo;
 using ll = long long;
 using ull = unsigned long long;
 
+TEST(BitTest, CeilPow2) {
+    ASSERT_EQ(0, ceil_pow2(0));
+    ASSERT_EQ(0, ceil_pow2(1));
+    ASSERT_EQ(1, ceil_pow2(2));
+    ASSERT_EQ(2, ceil_pow2(3));
+    ASSERT_EQ(2, ceil_pow2(4));
+    ASSERT_EQ(3, ceil_pow2(5));
+    ASSERT_EQ(3, ceil_pow2(6));
+    ASSERT_EQ(3, ceil_pow2(7));
+    ASSERT_EQ(3, ceil_pow2(8));
+    ASSERT_EQ(4, ceil_pow2(9));
+    ASSERT_EQ(30, ceil_pow2(1 << 30));
+    ASSERT_EQ(31, ceil_pow2((1 << 30) + 1));
+    ASSERT_EQ(31, ceil_pow2(std::numeric_limits<int>::max()));
+}
+
+TEST(BitTest, BSF) {
+    ASSERT_EQ(0, bsf(1U));
+    ASSERT_EQ(1, bsf(2U));
+    ASSERT_EQ(0, bsf(3U));
+    ASSERT_EQ(2, bsf(4U));
+    ASSERT_EQ(0, bsf(5U));
+    ASSERT_EQ(1, bsf(6U));
+    ASSERT_EQ(0, bsf(7U));
+    ASSERT_EQ(3, bsf(8U));
+    ASSERT_EQ(0, bsf(9U));
+    ASSERT_EQ(30, bsf(1U << 30));
+    ASSERT_EQ(0, bsf((1U << 31) - 1));
+    ASSERT_EQ(31, bsf(1U << 31));
+    ASSERT_EQ(0, bsf(std::numeric_limits<unsigned int>::max()));
+}
+
 TEST(BitTest, BSR) {
     ASSERT_EQ(0, bsr(1U));
     ASSERT_EQ(1, bsr(2U));
--- yosupo/bit.hpp
+++ yosupo/bit.hpp
@@ -15,13 +15,19 @@ int ceil_pow2(int n) {
 int bsf(unsigned int n) { return __builtin_ctz(n); }
 // @param n `1 <= n`
 // @return minimum non-negative `x` s.t. `(n & (1 << x)) != 0`
+int bsf(unsigned long n) { return __builtin_ctzl(n); }
+// @param n `1 <= n`
+// @return minimum non-negative `x` s.t. `(n & (1 << x)) != 0`
 int bsf(unsigned long long n) { return __builtin_ctzll(n); }
 
 // @param n `1 <= n`
 // @return maximum non-negative `x` s.t. `(n & (1 << x)) != 0`
-int bsr(unsigned int n) { return 31 - __builtin_clz(n); }
+int bsr(unsigned int n) { return 8 * sizeof(unsigned int) - 1 - __builtin_clz(n); }
 // @param n `1 <= n`
 // @return maximum non-negative `x` s.t. `(n & (1 << x)) != 0`
-int bsr(unsigned long long n) { return 63 - __builtin_clzll(n); }
+int bsr(unsigned long n) { return 8 * sizeof(unsigned long) - 1 - __builtin_clzl(n); }
+// @param n `1 <= n`
+// @return maximum non-negative `x` s.t. `(n & (1 << x)) != 0`
+int bsr(unsigned long long n) { return 8 * sizeof(unsigned long long) - 1 - __builtin_clzll(n); }
 
 }  // namespace yosupo
