ce4fd8554266cd7f66175e36f36cb584a64d32c7
Kohei Morita <yosupo06@gmail.com>
2021-01-31 16:33:56 +0900

    add bit

--- yosupo/bit.hpp
+++ yosupo/bit.hpp
@@ -23,6 +23,13 @@ int bsf(unsigned long n) { return __builtin_ctzl(n); }
 // @param n `1 <= n`
 // @return minimum non-negative `x` s.t. `(n & (1 << x)) != 0`
 int bsf(unsigned long long n) { return __builtin_ctzll(n); }
+// @param n `1 <= n`
+// @return minimum non-negative `x` s.t. `(n & (1 << x)) != 0`
+int bsf(unsigned __int128 n) {
+    unsigned long long low = (unsigned long long)(n);
+    unsigned long long high = (unsigned long long)(n >> 64);
+    return low ? __builtin_ctzll(low) : 64 + __builtin_ctzll(high);
+}
 
 // @param n `1 <= n`
 // @return maximum non-negative `x` s.t. `(n & (1 << x)) != 0`
@@ -39,5 +46,15 @@ int bsr(unsigned long n) {
 int bsr(unsigned long long n) {
     return 8 * (int)sizeof(unsigned long long) - 1 - __builtin_clzll(n);
 }
+// @param n `1 <= n`
+// @return minimum non-negative `x` s.t. `(n & (1 << x)) != 0`
+int bsr(unsigned __int128 n) {
+    unsigned long long low = (unsigned long long)(n);
+    unsigned long long high = (unsigned long long)(n >> 64);
+    return high ? 127 - __builtin_clzll(high) : 63 - __builtin_ctzll(low);
+}
+
+int popcnt(unsigned int n) { return __builtin_popcount(n); }
+int popcnt(unsigned long n) { return __builtin_popcountll(n); }
 
 }  // namespace yosupo
