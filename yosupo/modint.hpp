675fabc0899e734be75b406838346e58e85bae6f
Kohei Morita <yosupo06@gmail.com>
2021-03-14 03:09:56 +0900

    add modint

--- yosupo/modint.hpp
+++ yosupo/modint.hpp
@@ -2,11 +2,30 @@
 
 #include "atcoder/modint.hpp"
 
+#include <iostream>
+
 namespace atcoder {
 
 template <int MOD>
 std::ostream& operator<<(std::ostream& os, const static_modint<MOD>& x) {
     return os << x.val();
 }
+
+template <int ID>
+std::ostream& operator<<(std::ostream& os, const dynamic_modint<ID>& x) {
+    return os << x.val();
+}
+
+}  // namespace yosupo
+
+namespace yosupo {
+
+template <int MOD> using static_modint = atcoder::static_modint<MOD>;
+
+template <int ID> using dynamic_modint = atcoder::dynamic_modint<ID>;
+
+using modint998244353 = atcoder::modint998244353;
+using modint1000000007 = atcoder::modint1000000007;
+using modint = atcoder::modint;
 
 }
