63735d350113f694a2786590051ce7dbf6e0a572
Kohei Morita <yosupo06@gmail.com>
2021-09-21 06:20:34 +0900

    refactor matching

--- yosupo/matching.hpp
+++ yosupo/matching.hpp
@@ -1,92 +1,16 @@
 #pragma once
 
-#include <vector>
+#include <algorithm>
 #include <cassert>
 #include <queue>
-#include <algorithm>
+#include <vector>
 
-#include "atcoder/internal_queue.hpp"
+#include "atcoder/internal_queue"
 
 namespace yosupo {
 struct MaxMatching {
-    int n;
-    std::vector<std::vector<int>> g;
-
-    std::vector<int> mt;
-
-    using P = std::pair<int, int>;
-    std::vector<int> is_ev;
-    std::vector<int> gr_buf;
-
-    std::vector<P> nx;
-    int st;
-    int group(int x) {
-        if (gr_buf[x] == -1 || is_ev[gr_buf[x]] != st) return gr_buf[x];
-        return gr_buf[x] = group(gr_buf[x]);
-    }
-
-    void match(int p, int b) {
-        int d = mt[p];
-        mt[p] = b;
-        if (d == -1 || mt[d] != p) return;
-        if (nx[p].second == -1) {
-            mt[d] = nx[p].first;
-            match(nx[p].first, d);
-        } else {
-            match(nx[p].first, nx[p].second);
-            match(nx[p].second, nx[p].first);
-        }
-    }
-
-    bool arg() {
-        is_ev[st] = st;
-        gr_buf[st] = -1;
-        nx[st] = P(-1, -1);
-        std::queue<int> q;
-        q.push(st);
-        while (q.size()) {
-            int a = q.front();
-            q.pop();
-            for (int b : g[a]) {
-                if (b == st) continue;
-                if (mt[b] == -1) {
-                    mt[b] = a;
-                    match(a, b);
-                    return true;
-                }
-                if (is_ev[b] == st) {
-                    int x = group(a), y = group(b);
-                    if (x == y) continue;
-                    int z = -1;
-                    while (x != -1 || y != -1) {
-                        if (y != -1) std::swap(x, y);
-                        if (nx[x] == P(a, b)) {
-                            z = x;
-                            break;
-                        }
-                        nx[x] = P(a, b);
-                        x = group(nx[mt[x]].first);
-                    }
-                    for (int v : {group(a), group(b)}) {
-                        while (v != z) {
-                            q.push(v);
-                            is_ev[v] = st;
-                            gr_buf[v] = z;
-                            v = group(nx[mt[v]].first);
-                        }
-                    }
-                } else if (is_ev[mt[b]] != st) {
-                    is_ev[mt[b]] = st;
-                    nx[b] = P(-1, -1);
-                    nx[mt[b]] = P(a, -1);
-                    gr_buf[mt[b]] = b;
-                    q.push(mt[b]);
-                }
-            }
-        }
-        return false;
-    }
-    MaxMatching(int _n) : n(_n), g(_n), mt(n, -1), is_ev(n, -1), gr_buf(n), nx(n) {}
+    MaxMatching(int _n)
+        : n(_n), g(_n), mate(n, -1) {}
 
     void add_edge(int a, int b) {
         g[a].push_back(b);
@@ -94,25 +18,119 @@ struct MaxMatching {
     }
 
     std::vector<std::pair<int, int>> max_matching() {
-        for (int i = 0; i < n; i++) {
-            for (int j : g[i]) {
-                if (mt[i] == -1 && mt[j] == -1) {
-                    mt[i] = mt[j];
-                    mt[j] = mt[i];
-                }
-            }
-        }
-        for (st = 0; st < n; st++) {
-            if (mt[st] == -1) arg();
-        }
+        greedy_match();
+        blossom_match();
         std::vector<std::pair<int, int>> result;
         for (int i = 0; i < n; i++) {
-            if (i < mt[i]) {
-                result.push_back({i, mt[i]});
+            if (i < mate[i]) {
+                result.push_back({i, mate[i]});
             }
         }
         return result;
     }
+
+  private:
+    int n;
+    std::vector<std::vector<int>> g;
+
+    std::vector<int> mate; // (i, mate[i]) はマッチしている, もしくは mate[i] = -1
+
+    void greedy_match() {
+        for (int i = 0; i < n; i++) {
+            for (int j : g[i]) {
+                if (mate[i] == -1 && mate[j] == -1) {
+                    mate[i] = mate[j];
+                    mate[j] = mate[i];
+                }
+            }
+        }
+    }
+
+    void blossom_match() {
+        std::vector<int> is_even(n, -1); // (i -> is_even[i])への交互パスが存在
+        std::vector<int> _first(n); // 親方向
+        
+        // 代表偶点 nx[i] = (j, -1) : P(i) = (i, mate[i]) + (mate[i], j) + P(j)
+        // 非代表偶点 nx[i] = (j, k) : P(i) = P(j) + P(k) + 辺(j, k) - P(mate[i]) + (i, mate[i])
+        std::vector<std::pair<int, int>> nx(n);
+
+        auto match = [&](auto self, int p, int b) {
+            int d = mate[p];
+            mate[p] = b;
+            if (d == -1 || mate[d] != p) return;
+            if (nx[p].second == -1) {
+                mate[d] = nx[p].first;
+                self(self, nx[p].first, d);
+            } else {
+                self(self, nx[p].first, nx[p].second);
+                self(self, nx[p].second, nx[p].first);
+            }
+        };
+
+        // st を支点にM-増加パスを探索
+        auto argment = [&](int st) {
+            // 親の奇点を返す
+            auto first_rec = [&](auto self, int x) {
+                if (_first[x] == -1 || is_even[_first[x]] != st)
+                    return _first[x];
+                return _first[x] = self(self, _first[x]);
+            };
+            auto first = [&](int x) {
+                return first_rec(first_rec, x);
+            };
+
+            is_even[st] = st;
+            _first[st] = -1;
+            nx[st] = {-1, -1};
+            atcoder::internal::simple_queue<int> q;
+            q.push(st);
+            while (q.size()) {
+                int a = q.front();
+                q.pop();
+                for (int b : g[a]) {
+                    if (b == st) continue;
+                    if (mate[b] == -1) {
+                        mate[b] = a;
+                        match(match, a, b);
+                        return true;
+                    }
+                    if (is_even[b] == st) {
+                        int x = first(a), y = first(b);
+                        if (x == y) continue;
+                        int z = -1;
+                        while (x != -1 || y != -1) {
+                            if (y != -1) std::swap(x, y);
+                            if (nx[x] == std::make_pair(a, b)) {
+                                z = x;
+                                break;
+                            }
+                            nx[x] = {a, b};
+                            x = first(nx[mate[x]].first);
+                        }
+                        for (int v : {first(a), first(b)}) {
+                            while (v != z) {
+                                q.push(v);
+                                is_even[v] = st;
+                                _first[v] = z;
+                                v = first(nx[mate[v]].first);
+                            }
+                        }
+                    } else if (is_even[mate[b]] != st) {
+                        is_even[mate[b]] = st;
+                        nx[b] = {-1, -1};
+                        nx[mate[b]] = {a, -1};
+                        _first[mate[b]] = b;
+                        q.push(mate[b]);
+                    }
+                }
+            }
+            return false;
+        };
+
+        for (int st = 0; st < n; st++) {
+            if (mate[st] == -1) argment(st);
+        }        
+    }
 };
 
-}
+}  // namespace yosupo
