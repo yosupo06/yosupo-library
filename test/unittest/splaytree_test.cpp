128ebb95bfe715d83b10be9c6f1ddf730570d5de
Kohei Morita <yosupo06@gmail.com>
2023-12-11 02:49:19 +0900

    a

--- test/unittest/splaytree_test.cpp
+++ test/unittest/splaytree_test.cpp
@@ -12,12 +12,8 @@ struct RangeAddMax {
     S e() { return 0; }
     S rev(S a) { return a; }
 
-    S mapping(F l, S r) {
-        return l + r;
-    }
-    F composition(F l, F r) {
-        return l + r;
-    }
+    S mapping(F l, S r) { return l + r; }
+    F composition(F l, F r) { return l + r; }
     F id() { return 0; }
 };
 
@@ -25,6 +21,15 @@ TEST(SplayTreeTest, Build) {
     yosupo::SplayTree tree((RangeAddMax()));
     auto tr = tree.build({1, 2, 3, 4, 5});
     ASSERT_EQ(std::vector<int>({1, 2, 3, 4, 5}), tree.to_vec(tr));
+}
+
+TEST(SplayTreeTest, Split) {
+    yosupo::SplayTree tree((RangeAddMax()));
+    auto tr1 = tree.build({1, 2, 3, 4, 5});
+    auto tr2 = tree.split(tr1, 4);
+
+    ASSERT_EQ(std::vector<int>({1, 2, 3, 4}), tree.to_vec(tr1));
+    ASSERT_EQ(std::vector<int>({5}), tree.to_vec(tr2));
 }
 
 TEST(SplayTreeTest, MaxRight) {
