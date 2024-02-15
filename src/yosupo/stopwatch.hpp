62776e0378cc89a8508f82f28308e265afdaef98
Kohei Morita <yosupo06@gmail.com>
2024-02-16 04:42:01 +0900

    add stopwatch

---
+++ src/yosupo/stopwatch.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <chrono>
+
+namespace yosupo {
+
+struct StopWatch {
+    std::chrono::steady_clock::time_point begin;
+
+    StopWatch() : begin(std::chrono::steady_clock::now()) {}
+    
+    int msecs() {
+        auto now = std::chrono::steady_clock::now();
+        return int(duration_cast<std::chrono::milliseconds>(now - begin).count());
+    }
+};
+
+}
