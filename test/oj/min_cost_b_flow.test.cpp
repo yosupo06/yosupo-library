0edbc50b12051a63396f086594517e8a9c451b55
Kohei Morita <yosupo06@gmail.com>
2024-07-01 14:52:42 +0000

    add network simplex

---
+++ test/oj/min_cost_b_flow.test.cpp
@@ -0,0 +1,37 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/min_cost_b_flow
+#include "yosupo/fastio.hpp"
+#include "yosupo/networksimplex.hpp"
+
+yosupo::Scanner sc(stdin);
+yosupo::Printer pr(stdout);
+
+using ll = long long;
+int main() {
+    int n, m;
+    sc.read(n, m);
+    yosupo::NetworkSimplexGraph<ll, ll> g(n);
+    for (int i : std::views::iota(0, n)) {
+        ll b;
+        sc.read(b);
+        g.add_excess(i, b);
+    }
+    for (int _ : std::views::iota(0, m)) {
+        int s, t;
+        ll l, u, c;
+        sc.read(s, t, l, u, c);
+        g.add_edge(s, t, l, u, c);
+    }
+    auto r = solve<__int128>(std::move(g));
+
+    if (!r.feasible) {
+        pr.writeln("infeasible");
+        return 0;
+    }
+    pr.writeln(r.cost);
+    for (int i : std::views::iota(0, n)) {
+        pr.writeln(r.potential[i]);
+    }
+    for (int i : std::views::iota(0, m)) {
+        pr.writeln(r.flow[i]);
+    }
+}
---
+++ test/unittest/networksimplex_test.cpp
@@ -0,0 +1,29 @@
+#include "yosupo/networksimplex.hpp"
+
+#include "gtest/gtest.h"
+
+using namespace yosupo;
+
+TEST(NetworkSimplexTest, Usage) {
+    NetworkSimplexGraph<int, int> g(3);
+    g.add_edge(0, 1, 0, 3, 1);
+    g.add_edge(1, 2, 0, 10, 2);
+    g.add_excess(0, 2);
+    g.add_excess(2, -2);
+
+    auto r = solve<int>(g);
+    ASSERT_TRUE(r.feasible);
+    ASSERT_EQ(r.cost, 6);
+    ASSERT_EQ(r.flow, std::vector<int>({2, 2}));
+}
+
+TEST(NetworkSimplexTest, Infeasible) {
+    NetworkSimplexGraph<int, int> g(3);
+    g.add_edge(0, 1, 0, 3, 1);
+    g.add_edge(1, 2, 0, 10, 2);
+    g.add_excess(0, 10);
+    g.add_excess(2, -10);
+
+    auto r = solve<int>(g);
+    ASSERT_FALSE(r.feasible);
+}
