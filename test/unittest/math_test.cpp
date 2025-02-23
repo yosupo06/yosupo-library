fcbd1e59c64e74b1a4cdf5a732cfecdada244c9f
Kohei Morita <yosupo06@gmail.com>
2025-02-24 02:21:33 +0900

    add ceil_div, floor_div

--- test/unittest/math_test.cpp
+++ test/unittest/math_test.cpp
@@ -6,19 +6,6 @@ TEST(MathTest, Abs) {
     EXPECT_EQ(yosupo::abs((__int128)-123), 123);
 }
 
-TEST(MathTest, Prime) {
-    auto is_prime = [&](int x) {
-        if (x <= 1) return false;
-        for (int i = 2; i * i <= x; i++) {
-            if (x % i == 0) return false;
-        }
-        return true;
-    };
-    for (int i = 0; i < 10000; i++) {
-        ASSERT_EQ(yosupo::is_prime(i), is_prime(i));
-    }
-}
-
 TEST(MathTest, Gcd) {
     __int128 x = 1;
     x <<= 100;
@@ -40,5 +27,36 @@ TEST(MathTest, GcdStress) {
         for (int b = -100; b <= 100; b++) {
             ASSERT_EQ(yosupo::gcd(a, b), gcd(a, b));
         }
+    }
+}
+
+TEST(MathTest, FloorDiv) {
+    EXPECT_EQ(yosupo::floor_div(11, 5), 2);
+    EXPECT_EQ(yosupo::floor_div(10, 5), 2);
+    EXPECT_EQ(yosupo::floor_div(9, 5), 1);
+    EXPECT_EQ(yosupo::floor_div(-9, 5), -2);
+    EXPECT_EQ(yosupo::floor_div(-10, 5), -2);
+    EXPECT_EQ(yosupo::floor_div(-11, 5), -3);
+}
+
+TEST(MathTest, CeilDiv) {
+    EXPECT_EQ(yosupo::ceil_div(11, 5), 3);
+    EXPECT_EQ(yosupo::ceil_div(10, 5), 2);
+    EXPECT_EQ(yosupo::ceil_div(9, 5), 2);
+    EXPECT_EQ(yosupo::ceil_div(-9, 5), -1);
+    EXPECT_EQ(yosupo::ceil_div(-10, 5), -2);
+    EXPECT_EQ(yosupo::ceil_div(-11, 5), -2);
+}
+
+TEST(MathTest, Prime) {
+    auto is_prime = [&](int x) {
+        if (x <= 1) return false;
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) return false;
+        }
+        return true;
+    };
+    for (int i = 0; i < 10000; i++) {
+        ASSERT_EQ(yosupo::is_prime(i), is_prime(i));
     }
 }
