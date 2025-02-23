dd3ec1decc13b39ffdc4c696d847f66f16b06aaf
Kohei Morita <yosupo06@gmail.com>
2025-02-24 01:33:44 +0900

    add fraction

---
+++ src/yosupo/fraction.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <iostream>
+
+#include "yosupo/math.hpp"
+
+namespace yosupo {
+
+template <class T>
+concept Printable = requires(std::ostream& os, const T& t) { os << t; };
+
+template <class T>
+struct Fraction {
+    T a, b;
+
+    Fraction() : a(T(0)), b(T(1)) {}
+    Fraction(T x) : a(x), b(T(1)) {}
+
+    Fraction(T _a, T _b) {
+        T g = yosupo::gcd(_a, _b);
+        if (_b < 0) g = -g;
+        a = _a / g;
+        b = _b / g;
+    }
+    Fraction operator-() const { return {-a, b};}
+    Fraction operator+(const Fraction& r) const { return {r.b * a + b * r.a, b * r.b}; }
+    Fraction operator-(const Fraction& r) const { return *this + (-r); }
+    Fraction operator*(const Fraction& r) const { return {a * r.a, b * r.b}; }
+    Fraction operator/(const Fraction& r) const { return {a * r.b, b * r.a}; }
+    Fraction& operator+=(const Fraction& r) { return *this = *this + r; }
+    Fraction& operator-=(const Fraction& r) { return *this = *this - r; }
+    Fraction& operator*=(const Fraction& r) { return *this = *this * r; }
+    Fraction& operator/=(const Fraction& r) { return *this = *this / r; }
+
+    bool operator==(const Fraction& r) const { return a == r.a && b == r.b; }
+    bool operator!=(const Fraction& r) const { return !(*this == r); }
+    std::strong_ordering operator<=>(const Fraction& rhs) const {
+        return a * rhs.b <=> b * rhs.a;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, Fraction x)
+    requires Printable<T> {
+        return os << x.a << "/" << x.b;
+    }
+};
+}
