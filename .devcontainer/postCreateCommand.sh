67f008d17fc6c5ea799de10340457ef875cebd14
Kohei Morita <yosupo06@gmail.com>
2025-02-24 22:40:05 +0000

    upgrade devcontainer to 24.04

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,4 +6,4 @@ sudo apt-get update
 
 sudo apt-get install -y cmake
 
-pip install competitive-verifier online-judge-tools online-judge-verify-helper
+pipx install competitive-verifier online-judge-tools online-judge-verify-helper pre-commit
