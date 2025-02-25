8ed6c92aa5d6a8aa02440bc6708bed43f8c253a3
Kohei Morita <yosupo06@gmail.com>
2025-02-25 16:56:52 +0000

    add halfplane_intersection

--- src/yosupo/geo/line.hpp
+++ src/yosupo/geo/line.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+#include <deque>
 #include <iostream>
 
 #include "yosupo/geo/point.hpp"
@@ -23,6 +25,48 @@ std::pair<int, Point<T>> cross_ll(const Line<T>& l, const Line<T>& r) {
         return {-1, {}};
     }
     return {1, r.s + r.vec() * cr2 / cr1};
+}
+
+// 半平面の共通部分を求める、半平面はLine lによってccw(l.s, l.t, x) !=
+// -1となる点の集合
+template <class T>
+std::vector<Line<T>> halfplane_intersection(std::vector<Line<T>> lines) {
+    std::ranges::sort(lines, [&](const auto& a, const auto& b) {
+        auto c = cmp_arg(a.vec(), b.vec());
+        if (c != 0) return c < 0;
+        // 角度が同じ場合は半平面が大きい方を前に
+        return ccw(a.s, a.t, b.s) == 1;
+    });
+    lines.erase(unique(lines.begin(), lines.end(),
+                       [&](const auto& a, const auto& b) {
+                           return cmp_arg(a.vec(), b.vec()) == 0;
+                       }),
+                lines.end());
+
+    std::deque<Line<T>> st;
+    for (const auto& l : lines) {
+        bool err = false;
+        auto is_need = [&](const Line<T>& a, const Line<T>& b,
+                           const Line<T>& c) {
+            T ab_dw = crs(a.vec(), b.vec()), ab_up = crs(a.vec(), a.t - b.s);
+            T bc_dw = crs(b.vec(), c.vec()), bc_up = crs(c.t - b.s, c.vec());
+            if (sgn(ab_dw) <= 0 || sgn(bc_dw) <= 0) return true;
+            bool f = bc_up * ab_dw > bc_dw * ab_up;
+            if (!f && sgn(crs(a.vec(), c.vec())) < 0) err = true;
+            return f;
+        };
+        while (st.size() >= 2 && !is_need(l, st[0], st[1])) st.pop_front();
+        while (st.size() >= 2 &&
+               !is_need(st[st.size() - 2], st[st.size() - 1], l))
+            st.pop_back();
+        if (st.size() < 2 || is_need(st.back(), l, st.front())) st.push_back(l);
+        if (err) return {};
+    }
+    if (st.size() == 2 && !sgn(crs(st[0].vec(), st[1].vec())) &&
+        sgn(crs(st[0].vec(), st[1].s - st[0].s) <= 0))
+        return {};
+
+    return {st.begin(), st.end()};
 }
 
 }  // namespace yosupo
--- test/unittest/geo/line_test.cpp
+++ test/unittest/geo/line_test.cpp
@@ -9,7 +9,7 @@ using R = Fraction<int>;
 using P = Point<R>;
 using L = Line<R>;
 
-TEST(LineTest, CrossLL_Intersecting) {
+TEST(LineTest, CrossLLIntersecting) {
     L l1{P{0, 0}, P{1, 1}};
     L l2{P{0, 1}, {1, 0}};
     auto res1 = cross_ll(l1, l2);
@@ -17,16 +17,21 @@ TEST(LineTest, CrossLL_Intersecting) {
     ASSERT_EQ(res1.second.x, R(1, 2));
     ASSERT_EQ(res1.second.y, R(1, 2));
 }
-
-TEST(LineTest, CrossLL_Parallel) {
+TEST(LineTest, CrossLLParallel) {
     L l3{{0, 0}, {1, 0}};
     L l4{{0, 1}, {1, 1}};
     auto res2 = cross_ll(l3, l4);
     ASSERT_EQ(res2.first, 0);
 }
-TEST(LineTest, CrossLL_Collinear) {
+TEST(LineTest, CrossLLCollinear) {
     L l5{{0, 0}, {1, 1}};
     L l6{{2, 2}, {3, 3}};
     auto res3 = cross_ll(l5, l6);
     ASSERT_EQ(res3.first, -1);
+}
+
+TEST(LineTest, HalfplaneIntersectsEmpty) {
+    std::vector<L> lines = {L{{0, 0}, {10, 0}}, L{{10, -1}, {0, -1}}};
+    auto res = halfplane_intersection(lines);
+    ASSERT_TRUE(res.empty());
 }
