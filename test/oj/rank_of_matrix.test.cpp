28fa73a7009bdfbf57f100ccac7a7058a083ed58
Kohei Morita <yosupo06@gmail.com>
2025-03-08 08:22:10 +0000

    add matrix rank

---
+++ test/oj/rank_of_matrix.test.cpp
@@ -0,0 +1,26 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/matrix_rank
+
+#include "atcoder/modint.hpp"
+#include "yosupo/fastio.hpp"
+#include "yosupo/matrix.hpp"
+#include "yosupo/vector2d.hpp"
+
+yosupo::Scanner sc(stdin);
+yosupo::Printer pr(stdout);
+using mint = atcoder::modint998244353;
+
+int main() {
+    int n, m;
+    sc.read(n, m);
+
+    yosupo::Vector2D<mint> a(n, m);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            int x;
+            sc.read(x);
+            a[{i, j}] = x;
+        }
+    }
+
+    pr.writeln(yosupo::rank(a));
+}
