31cd3c8391e06852b8c73139788b40813ad3d94d
Kohei Morita <yosupo06@gmail.com>
2025-03-18 08:49:06 +0000

    change data structure of coord

--- src/yosupo/coord.hpp
+++ src/yosupo/coord.hpp
@@ -7,15 +7,21 @@
 namespace yosupo {
 
 struct Coord {
-    int r, c;
+    int d[2];
 
-    Coord() : r(0), c(0) {}
-    Coord(int _r, int _c) : r(_r), c(_c) {}
-    Coord(const std::pair<int, int>& p) : r(p.first), c(p.second) {}
+    Coord() : d{0, 0} {}
+    Coord(int _r, int _c) : d{_r, _c} {}
+    Coord(const std::pair<int, int>& p) : d{p.first, p.second} {}
+
+    int& r() { return d[0]; }
+    int& c() { return d[1]; }
+
+    const int& r() const { return d[0]; }
+    const int& c() const { return d[1]; }
 
     Coord& operator+=(const Coord& other) {
-        r += other.r;
-        c += other.c;
+        d[0] += other.d[0];
+        d[1] += other.d[1];
         return *this;
     }
 
@@ -26,8 +32,8 @@ struct Coord {
     }
 
     Coord& operator-=(const Coord& other) {
-        r -= other.r;
-        c -= other.c;
+        d[0] -= other.d[0];
+        d[1] -= other.d[1];
         return *this;
     }
 
@@ -38,21 +44,21 @@ struct Coord {
     }
 
     int& operator[](int index) {
-        if (index == 0) return r;
-        if (index == 1) return c;
+        if (index == 0) return d[0];
+        if (index == 1) return d[1];
         assert(false);
     }
 
     const int& operator[](int index) const {
-        if (index == 0) return r;
-        if (index == 1) return c;
+        if (index == 0) return d[0];
+        if (index == 1) return d[1];
         assert(false);
     }
 
-    operator std::pair<int, int>() const { return std::make_pair(r, c); }
+    operator std::pair<int, int>() const { return std::make_pair(d[0], d[1]); }
 
     std::string dump() const {
-        return "(" + std::to_string(r) + ", " + std::to_string(c) + ")";
+        return "(" + std::to_string(d[0]) + ", " + std::to_string(d[1]) + ")";
     }
 };
 
