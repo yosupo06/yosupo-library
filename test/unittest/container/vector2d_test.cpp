ab20334528e12865fa6515404713035bff915581
Kohei Morita <yosupo06@gmail.com>
2025-12-09 18:25:56 +0000

    refactor vector2d

--- test/unittest/container/vector2d_test.cpp
+++ test/unittest/container/vector2d_test.cpp
@@ -2,6 +2,8 @@
 
 #include <gtest/gtest.h>
 
+#include <utility>
+
 using namespace yosupo;
 
 TEST(Vector2DTest, Empty) {
@@ -145,4 +147,76 @@ TEST(Vector2DTest, Fill) {
     ASSERT_EQ((a[{1, 0}]), 3);
     a.fill(111);
     ASSERT_EQ((a[{0, 2}]), 111);
+}
+
+TEST(Vector2DTest, MoveAndSwap) {
+    Vector2D<int> a(2, 2, 3);
+    Vector2D<int> b(1, 1, 9);
+
+    auto a_ptr = a.data();
+    auto b_ptr = b.data();
+
+    Vector2D<int> moved = std::move(a);
+    EXPECT_EQ(moved.data(), a_ptr);
+    EXPECT_EQ(moved.h, 2);
+    EXPECT_EQ(moved.w, 2);
+    EXPECT_EQ((moved[{1, 1}]), 3);
+    EXPECT_EQ(a.data(), nullptr);
+    EXPECT_EQ(a.h, 0);
+    EXPECT_EQ(a.w, 0);
+
+    std::swap(moved, b);
+    EXPECT_EQ(moved.data(), b_ptr);
+    EXPECT_EQ(moved.h, 1);
+    EXPECT_EQ(moved.w, 1);
+    EXPECT_EQ((moved[{0, 0}]), 9);
+    EXPECT_EQ(b.data(), a_ptr);
+    EXPECT_EQ(b.h, 2);
+    EXPECT_EQ(b.w, 2);
+    EXPECT_EQ((b[{0, 0}]), 3);
+}
+
+TEST(Vector2DTest, Transpose) {
+    Vector2D<int> a = {{1, 2, 3}, {4, 5, 6}};
+    auto t = a.transpose();
+    ASSERT_EQ(t.h, 3);
+    ASSERT_EQ(t.w, 2);
+    ASSERT_EQ((t[{0, 0}]), 1);
+    ASSERT_EQ((t[{0, 1}]), 4);
+    ASSERT_EQ((t[{1, 0}]), 2);
+    ASSERT_EQ((t[{1, 1}]), 5);
+    ASSERT_EQ((t[{2, 0}]), 3);
+    ASSERT_EQ((t[{2, 1}]), 6);
+}
+
+TEST(Vector2DTest, SwapAndArithmetic) {
+    Vector2D<int> a = {{1, 2}, {3, 4}};
+    Vector2D<int> b = {{5, 6}, {7, 8}};
+    std::swap(a, b);
+    ASSERT_EQ((a[{0, 0}]), 5);
+    ASSERT_EQ((a[{1, 1}]), 8);
+    ASSERT_EQ((b[{0, 0}]), 1);
+    ASSERT_EQ((b[{1, 1}]), 4);
+
+    auto c = a + b;
+    ASSERT_EQ((c[{0, 0}]), 6);
+    ASSERT_EQ((c[{1, 1}]), 12);
+
+    c -= b;
+    ASSERT_EQ(c, a);
+}
+
+TEST(Vector2DTest, Pow) {
+    Vector2D<int> a = {{1, 1}, {1, 0}};
+    auto a2 = a.pow(2);
+    ASSERT_EQ((a2[{0, 0}]), 2);
+    ASSERT_EQ((a2[{0, 1}]), 1);
+    ASSERT_EQ((a2[{1, 0}]), 1);
+    ASSERT_EQ((a2[{1, 1}]), 1);
+
+    auto a5 = a.pow(5);
+    ASSERT_EQ((a5[{0, 0}]), 8);
+    ASSERT_EQ((a5[{0, 1}]), 5);
+    ASSERT_EQ((a5[{1, 0}]), 5);
+    ASSERT_EQ((a5[{1, 1}]), 3);
 }
